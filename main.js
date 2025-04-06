5111f7c93efb2a2108a7af7ef82a0783196a1577
himanshu1081 <149322620+himanshu1081@users.noreply.github.com>
2025-04-06 15:12:35 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 const click = document.querySelectorAll(".clicked");
 
+
+
 function display(choice, computerChoice, Wins, Loses, Ties, result) {
-    alert(`You chose ${choice} and computer chose ${computerChoice}. You ${result}\n
-Wins: ${Wins},Loses: ${Loses},Ties:${Ties}`);
+    document.querySelector('.score')
+        .innerHTML=`Wins: ${Wins},Loses: ${Loses},Ties:${Ties}
+        <br>Your Choice: ${choice}<br> Computer Choice: ${computerChoice}<br>
+        It's a ${result}`;
 }
 
 let score = JSON.parse(localStorage.getItem('score')) || {
@@ -10,16 +14,21 @@ let score = JSON.parse(localStorage.getItem('score')) || {
     Loses: 0,
     Ties: 0
 };
+
+function colorChange(result){
+    document.body.style.backgroundColor=result;
+}
+
 function reset() {
     score.Wins = 0;
     score.Loses = 0;
     score.Ties = 0;
-    alert("Score Reset Successful!")
+    document.querySelector('.score')
+        .innerHTML="Wins: 0,Loses: 0,Ties:0";
 }
 function clicked(event) {
     let computerChoice = '';
     let number = Math.random();
-
 
     if (number < 1 / 3) {
         computerChoice = 'Rock';
@@ -32,18 +41,24 @@ function clicked(event) {
     let choice = event.target.innerText;
     if (choice == computerChoice) {
         score.Ties++;
+        colorChange('grey');
         display(choice, computerChoice, score.Wins, score.Loses, score.Ties, 'Tie');
     } else if (choice == 'Reset') {
         reset();
+        colorChange('#f4f4f4');
     } else if ((choice == 'Paper' && computerChoice == 'Rock') || (choice == 'Rock' && computerChoice == 'Scissors') || (choice == 'Scissors' && computerChoice == 'Paper')) {
         score.Wins++;
-        display(choice, computerChoice, score.Wins, score.Loses, score.Ties, 'Won');
+        colorChange('green');
+        display(choice, computerChoice, score.Wins, score.Loses, score.Ties, 'Win');
     } else {
         score.Loses++;
+        colorChange('red');
         display(choice, computerChoice, score.Wins, score.Loses, score.Ties, 'Lose');
     }
 
     localStorage.setItem('score', JSON.stringify(score));
+    
+    
 
 }
 
